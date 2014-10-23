b1cf5f2f5dc0e40d56e1d1d14ba1acfeb8511310
mohdsanadzakirizvi <mohdsanadzakirizvi@users.noreply.github.com>
2014-10-23 11:20:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ else if(count ==3)
 context.fillStyle="orange";
 else if(count >3)
 count=0;
-context.fillRect(xPos-165,yPos-165,w,h);},false);
+context.fillRect(xPos-155,yPos-165,w,h);},false);
 	
 
 }
